c8bb6f8b75ae7e9785e1362e8ca0a20384ee4b00
james-burgess <jms.bgr@gmail.com>
2017-03-08 22:02:48 +0200

    cleaned up the code and added comments

--- circleSquareTriangle.py
+++ circleSquareTriangle.py
@@ -1,32 +1,33 @@
-import turtle
+import turtle               #imports turtle
 
-j = turtle.Turtle()
-wn = turtle.Screen()
-x = 2
+j = turtle.Turtle()         #initaites turtle with name j
+wn = turtle.Screen()        #initates screen with name wn
 
-def circle(x):
-    for i in range(360):
-        j.forward(x)
+
+def circle(dist):           #function repeats forwardleft 360 times
+    for i in range(360):    #takes in argument dist which sets the size
+        j.forward(dist)
         j.left(1)
-
-def move(dis, ang):
+        
+def move(dis, ang):         #function takes in a distance and direction to move the pen
     j.penup()
     j.left(ang)
     j.forward(dis)
     j.pendown()
 
-def square(dis):
+def square(dis):            #draws square takes in size as dist
     for i in range(4):
         j.forward(dis)
         j.left(90)
 
-def triange(dis):
+def triange(dis):           #draws triangle takes in size as dist
     for i in range(3):
         j.forward(dis)
         j.left(120)
 
-circle(x)
-move(100,180)
-square(150)
-move(250, 180)
-triange(150)
+
+circle(2)       #calls circle of size 2
+move(100,180)   #moves back 100 steps
+square(150)     #call square size 150   
+move(250, 180)  #move 250 backwards
+triange(150)    #draw triangle size 150
